0bd4f2baedc695e663372762beb65dc5677ba486
Aaron Price <price.aaron@gmail.com>
2025-07-31 16:12:27 -0400

    Enable Sidekiq

--- Gemfile
+++ Gemfile
@@ -30,6 +30,9 @@ gem "solid_cache"
 gem "solid_queue"
 gem "solid_cable"
 
+# Background job processing
+gem "sidekiq"
+
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
 
--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module Zayzoon
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Use Sidekiq as the Active Job backend
+    config.active_job.queue_adapter = :sidekiq
   end
 end
