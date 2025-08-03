d23f380f8702b76a71d679af2753bdfc0dab0e07
Aaron Price <price.aaron@gmail.com>
2025-08-03 14:21:24 -0400

    Remove INFO logs from tests runs

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,18 @@
 # Configure Sidekiq
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV.fetch('REDIS_URL', 'redis://redis:6379/0') }
+
+  # Set log level to WARN in test environment to suppress INFO messages
+  if Rails.env.test?
+    config.logger.level = Logger::WARN
+  end
 end
 
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV.fetch('REDIS_URL', 'redis://redis:6379/0') }
+
+  # Set log level to WARN in test environment to suppress INFO messages
+  if Rails.env.test?
+    config.logger.level = Logger::WARN
+  end
 end
